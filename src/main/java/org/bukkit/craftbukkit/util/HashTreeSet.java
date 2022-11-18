65b03c0a92bdef77ccb2d3c159e60b70b78eddc2
BuildTools <unconfigured@null.spigotmc.org>
2022-11-18 22:18:43 +0800

    Optimize schedule ticks

--- src/main/java/org/bukkit/craftbukkit/util/HashTreeSet.java
+++ src/main/java/org/bukkit/craftbukkit/util/HashTreeSet.java
@@ -11,44 +11,40 @@ public class HashTreeSet<V> implements Set<V> {
     private HashSet<V> hash = new HashSet<V>();
     private TreeSet<V> tree = new TreeSet<V>();
 
-    public HashTreeSet() {
-
-    }
-
     @Override
-    public int size() {
+    public synchronized int size() {
         return hash.size();
     }
 
     @Override
-    public boolean isEmpty() {
+    public synchronized boolean isEmpty() {
         return hash.isEmpty();
     }
 
     @Override
-    public boolean contains(Object o) {
+    public synchronized boolean contains(Object o) {
         return hash.contains(o);
     }
 
     @Override
-    public Iterator<V> iterator() {
+    public synchronized Iterator<V> iterator() {
         return new Iterator<V>() {
 
             private Iterator<V> it = tree.iterator();
             private V last;
 
             @Override
-            public boolean hasNext() {
+            public synchronized boolean hasNext() {
                 return it.hasNext();
             }
 
             @Override
-            public V next() {
+            public synchronized V next() {
                 return last = it.next();
             }
 
             @Override
-            public void remove() {
+            public synchronized void remove() {
                 if (last == null) {
                     throw new IllegalStateException();
                 }
@@ -60,57 +56,57 @@ public class HashTreeSet<V> implements Set<V> {
     }
 
     @Override
-    public Object[] toArray() {
+    public synchronized Object[] toArray() {
         return hash.toArray();
     }
 
     @Override
-    public Object[] toArray(Object[] a) {
+    public synchronized Object[] toArray(Object[] a) {
         return hash.toArray(a);
     }
 
     @Override
-    public boolean add(V e) {
+    public synchronized boolean add(V e) {
         hash.add(e);
         return tree.add(e);
     }
 
     @Override
-    public boolean remove(Object o) {
+    public synchronized boolean remove(Object o) {
         hash.remove(o);
         return tree.remove(o);
     }
 
     @Override
-    public boolean containsAll(Collection c) {
+    public synchronized boolean containsAll(Collection c) {
         return hash.containsAll(c);
     }
 
     @Override
-    public boolean addAll(Collection c) {
+    public synchronized boolean addAll(Collection c) {
         tree.addAll(c);
         return hash.addAll(c);
     }
 
     @Override
-    public boolean retainAll(Collection c) {
+    public synchronized boolean retainAll(Collection c) {
         tree.retainAll(c);
         return hash.retainAll(c);
     }
 
     @Override
-    public boolean removeAll(Collection c) {
+    public synchronized boolean removeAll(Collection c) {
         tree.removeAll(c);
         return hash.removeAll(c);
     }
 
     @Override
-    public void clear() {
+    public synchronized void clear() {
         hash.clear();
         tree.clear();
     }
 
-    public V first() {
+    public synchronized V first() {
         return tree.first();
     }
 
