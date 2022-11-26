b9224cd947c4028a6772f8025ca0d8ccebcdfd17
BuildTools <unconfigured@null.spigotmc.org>
2022-11-26 22:04:18 +0800

    Fix class cast exception in touhoulittlemaid

--- src/main/java/gg/m2ke4u/skylight/ConcurrentCastableNonLinkedHashSet.java
+++ src/main/java/gg/m2ke4u/skylight/ConcurrentCastableNonLinkedHashSet.java
@@ -1,12 +1,14 @@
 package gg.m2ke4u.skylight;
 
+import com.google.common.collect.Sets;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
+import java.util.Set;
 import java.util.concurrent.ConcurrentSkipListSet;
 
-public class ConcurrentCastableLinkedHashSet<T> extends LinkedHashSet<T>{
-    private final ConcurrentSkipListSet<T> baking = new ConcurrentSkipListSet<>();
+public class ConcurrentCastableNonLinkedHashSet<T> extends LinkedHashSet<T>{
+    private final Set<T> baking = Sets.newConcurrentHashSet();
 
     @Override
     public int size() {
