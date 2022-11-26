5f21a8b904d5dcf444e4a9e1a4a5b4f51b56b1c4
BuildTools <unconfigured@null.spigotmc.org>
2022-11-26 21:47:03 +0800

    Fix class cast exception in touhoulittlemaid

---
+++ src/main/java/gg/m2ke4u/skylight/ConcurrentCastableLinkedHashSet.java
@@ -0,0 +1,85 @@
+package gg.m2ke4u.skylight;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.concurrent.ConcurrentSkipListSet;
+
+public class ConcurrentCastableLinkedHashSet<T> extends LinkedHashSet<T>{
+    private final ConcurrentSkipListSet<T> baking = new ConcurrentSkipListSet<>();
+
+    @Override
+    public int size() {
+        return this.baking.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.baking.isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return this.baking.contains(o);
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return this.baking.iterator();
+    }
+
+    @Override
+    public Object[] toArray() {
+        return this.baking.toArray();
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        return this.baking.toArray(a);
+    }
+
+    @Override
+    public boolean add(T t) {
+        return this.baking.add(t);
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return this.baking.remove(o);
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return this.baking.containsAll(c);
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        return this.baking.add((T) c);
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return this.baking.retainAll(c);
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return this.baking.removeAll(c);
+    }
+
+    @Override
+    public void clear() {
+        this.baking.clear();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return this.baking.equals(o);
+    }
+
+    @Override
+    public int hashCode() {
+        return this.baking.hashCode();
+    }
+}
